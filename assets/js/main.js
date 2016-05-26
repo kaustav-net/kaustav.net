fbe2d5fd42bd0747ea2ece630abdabffe947cff6
Kaustav Ghosh <kaustavghosh@Kaustavs-MacBook-Pro.local>
2016-05-26 21:52:30 +0530

    Update final scrolling effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,34 @@
-jQuery(document).ready(function($) {
+/*jQuery(document).ready(function($) {
 
 
-    /*======= Skillset *=======*/
+           
     
-    
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
-        $('.skills-section').waypoint(function() {
-            $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-        });
-         }, {
-           offset: '100%'
-         });
-    });
-   
-    
-
 });
+*/
+$(function() {
+	 //======= Skillset *=======
+		    
+		    
+	$('.level-bar-inner').css('width', '0');
+
+	
+    $('#mydiv').waypoint(function() {
+    	$('.level-bar-inner').each(function() {
+            
+        	var itemWidth = $(this).data('level');
+        
+        	$(this).animate({
+           		width: itemWidth
+        	}, 800);
+        });//window.location.href = 'http://google.com';
+
+    	}, {
+        offset: '70%'
+     });
+    
+    $('#top-div').waypoint(function() {
+    		$('.level-bar-inner').css('width', '0');
+    	}, {
+        offset: '0%'
+     });
+ });
