e12677f22d3fa1499f741d06deabe2fbc0e284da
Kaustav Ghosh <kaustav.ghosh28@hotmail.com>
2020-03-16 00:31:58 +0200

    Update cokkie consent message

--- js/cookiepolicy.js
+++ js/cookiepolicy.js
@@ -10,6 +10,6 @@ window.cookieconsent.initialise({
     "showLink": false,
     "position": "bottom-right",
     "content": {
-      "message": "This website uses cookies to ensure you get the best experience on this website."
+      "message": "This website uses cookies to ensure you get the best experience on this website. By using this website, you agree to our use of cookies."
     }
   });
